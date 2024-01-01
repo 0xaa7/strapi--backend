53bdf5159203323500b284fadcd2500c8f290f4b
Aamsher <hpandian008@gmail.com>
2024-01-01 21:35:10 +0530

    new changes

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,18 @@
-module.exports = {
-  settings: {
-    cors: {
-      enabled: true,
+module.exports = [
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
       origin: ["https://strapi-zjxa.onrender.com"],
+      // Add any other cors configuration options if needed
     },
   },
-};
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
